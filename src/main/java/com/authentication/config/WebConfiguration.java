f54578254a294e19527d5a06dd40006c3ed0c070
Rishabh <rishabhrawat.171105@gmail.com>
2024-12-01 19:35:10 +0530

    Fixed Oauth2 authentication error

--- src/main/java/com/authentication/config/WebConfiguration.java
+++ src/main/java/com/authentication/config/WebConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,20 +20,17 @@ import static org.springframework.security.config.Customizer.withDefaults;
 //@EnableMethodSecurity(prePostEnabled = true)
 public class WebConfiguration {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        (authorizeRequests) ->
-                                authorizeRequests
-//                                        .requestMatchers("/").permitAll()
-                                        .requestMatchers("/api/auth/register").permitAll()
-                                        .requestMatchers("/api/auth/login").permitAll()
-                                        .anyRequest().authenticated()
-                ).formLogin(withDefaults());
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers("/api/auth/register")
+						.permitAll().requestMatchers("/api/auth/login").permitAll().anyRequest().authenticated()
+						)
+                .formLogin(formLogin->formLogin.loginPage("/login").permitAll())
+                .oauth2Login(oauthLogin->oauthLogin.loginPage("/login").permitAll());
 
-//        http.oauth2Login(withDefaults());
-        return http.build();
-    }
+		return http.build();
+	}
+
+	
 }
